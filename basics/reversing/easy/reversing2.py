f14021661aaa7df0d1fc28ac3fdf22299da52ffb
Sarvesh <85302050+SarveshAadhithya@users.noreply.github.com>
2023-12-04 15:37:51 +0530

    Update reversing2.py

--- basics/reversing/easy/reversing2.py
+++ basics/reversing/easy/reversing2.py
@@ -21,17 +21,7 @@ a[94]+a[79]+a[64]+a[82]+a[82]+a[86]+a[78]+a[81]+a[67]+a[94]+\
 a[69]+a[78]+a[81]+a[94]+a[69]+a[75]+a[64]+a[70]+a[25]+a[94])
 def arg132():
   return 1
-def arg112():
-  print(a[54]+a[68]+a[75]+a[66]+a[78]+a[76]+a[68]+a[94]+a[65]+a[64]+a[66]+\
-a[74]+a[13]+a[13]+a[13]+a[94]+a[88]+a[78]+a[84]+a[81]+a[94]+a[69]+\
-a[75]+a[64]+a[70]+a[11]+a[94]+a[84]+a[82]+a[68]+a[81]+a[25])
-def arg122(arg432, arg423):
-    arg433 = arg423
-    i = 0
-    while len(arg433) < len(arg432):
-        arg433 = arg433 + arg423[i]
-        i = (i + 1) % len(arg423)        
-    return "".join([chr(ord(arg422) ^ ord(arg442)) for (arg422,arg442) in zip(arg432,arg433)])
+
 arg444 = arg132()
 arg432 = arg232()
 arg133(arg432)
